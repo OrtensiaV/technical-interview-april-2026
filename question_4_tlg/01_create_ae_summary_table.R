8412e5b35dc6ad73a5c348b07b3dcd77a392dc4c
Ortensia Vito <ortensia_90@hotmail.it>
2026-04-18 12:14:15 +0100

    First task of question 4 completed

---
+++ question_4_tlg/01_create_ae_summary_table.R
@@ -0,0 +1,35 @@
+# ============================================================================
+# Question 4: TLG - Adverse Event Reporting (Script 1)
+# ============================================================================
+#
+# Creat a summary table of treatment-emergent adverse events (TEAEs)
+#
+# Author: Ortensia Vito
+# ============================================================================
+
+setwd("~/Desktop/GitHub/technical-interview-april-2026/question_4_tlg")
+
+# Load required packages
+library(dplyr)
+library(pharmaverseadam)
+library(gtsummary)
+
+adae <- pharmaverseadam::adae
+adsl <- pharmaverseadam::adsl
+
+# Filter for treatment-emergent adverse events
+adae <- adae %>%
+  filter(TRTEMFL == "Y")
+           
+tbl_aesoc <- adae %>%
+  tbl_hierarchical(
+    variables = c(AESOC, AEDECOD),
+    by = ACTARM,
+    id = USUBJID,
+    denominator = adsl,
+    overall_row = TRUE,
+    label = "..ard_hierarchical_overall.." ~ "Any SAE"
+  ) %>%
+  as_gt() %>% 
+  gt::gtsave(filename = "tbl_aesoc.html")
+

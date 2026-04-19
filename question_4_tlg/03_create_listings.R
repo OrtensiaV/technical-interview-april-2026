e75a5c3d5fe11cfeeb0079e2354d1779fcc4ad20
Ortensia Vito <32670488+OrtensiaV@users.noreply.github.com>
2026-04-19 18:56:27 +0100

    Update 03_create_listings.R

--- question_4_tlg/03_create_listings.R
+++ question_4_tlg/03_create_listings.R
@@ -12,6 +12,7 @@ library(dplyr)
 library(pharmaverseadam)
 library(gtsummary)
 library(gtreg)
+library(gt)
 
 setwd("~/Desktop/GitHub/technical-interview-april-2026/question_4_tlg")
 
@@ -20,11 +21,23 @@ adae <- pharmaverseadam::adae
 
 adae %>%
   filter(TRTEMFL %in% "Y") %>%
-  dplyr::select(USUBJID, ACTARM, AETERM, AESEV, AEREL, AESTDTC, AEENDTC) %>%
   arrange(USUBJID, AESTDTC) %>%
+  group_by(USUBJID) %>%
+  mutate(
+    USUBJID_DISPLAY = if_else(row_number() == 1, as.character(USUBJID), ""),
+    ACTARM_DISPLAY = if_else(row_number() == 1, as.character(ACTARM), "")
+  ) %>%
+  ungroup() %>%
+  mutate(
+    USUBJID_DISPLAY = structure(USUBJID_DISPLAY, label = attr(USUBJID, "label")),
+    ACTARM_DISPLAY = structure(ACTARM_DISPLAY, label = attr(ACTARM, "label"))
+  ) %>%
+  dplyr::select(USUBJID_DISPLAY, ACTARM_DISPLAY, AETERM, AESEV, AEREL, AESTDTC, AEENDTC) %>%
   tbl_listing() %>%
-  as_gt() %>% 
+  as_gt() %>%
+  gt::tab_options(
+    table_body.hlines.style = "none"
+  ) %>%
   gt::gtsave(filename = "ae_listings.html")
-  
   
   
